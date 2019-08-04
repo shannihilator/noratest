d7376c45a92c4e629fa90a2e6423663c1d746955
anonymous <anonymous>
2019-08-04 00:43:59 -0400

    Update App.js
    added comments for code

--- src/App.js
+++ src/App.js
@@ -4,18 +4,24 @@ import './App.css';
 import axios from 'axios'
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css"
 import Loader from 'react-loader-spinner'
-let USER_TOKEN = process.env.REACT_APP_TOKEN;
-let URL = 'https://api.salesloft.com/v2/people.json'
-const AuthStr = 'Bearer ' + USER_TOKEN;
+
+// removes cors error
 const proxyurl = "https://cors-anywhere.herokuapp.com/";
+let SalesLoft_API_TOKEN = process.env.REACT_APP_TOKEN;
+let SalesLoftURL = 'https://api.salesloft.com/v2/people.json'
+const AuthStr = 'Bearer ' + SalesLoft_API_TOKEN;
+
 class App extends Component {
 
   state = {
     people: [],
     show: false
   }
+
   componentDidMount() {
-    axios.get(proxyurl + URL, { 'headers': { 'Authorization': AuthStr } })
+//     grapping people data from SalesLoft API
+    axios.get(`${proxyurl}${SalesLoftURL}`, { 'headers': { 'Authorization': AuthStr } })
+//     storing them within state
     .then(res => this.setState({
       ...this.state,
       people: res.data
@@ -25,24 +31,28 @@ class App extends Component {
 
   handleClick = (e) => {
     e.preventDefault();
+//     updating show on show count btn click
+//     shows list of the highest counted letters in email
     this.setState({
       ...this.state,
       show: !this.state.show
     })
   }
   render() {
-    const {data, metadata} = this.state.people;
-    if (data) {
+    const {people, metadata} = this.state.people;
+    if (people) {
       return (
         <div className="cardContainer">
           <button onClick={(e) => this.handleClick(e)} className="btn">Show Count</button>
-          {data.map(person => {
+//           mapping through peoples array and creating a card 
+          {people.map(person => {
               return <Card key={person.id} person={person} show={this.state.show}/>
           })}
         </div>
         
     )
     } else {
+//       loader while api call is being made
       return  <div style={{display: "flex", justifyContent: "center", marginTop: "10%"}} ><Loader  className='loader' type="Puff" color="#00BFFF" height={300} width={300}/></div>
     }
   }
